24d45ac948907e7f50e11fd9a3410d50856b10de
duartecgustavo@outlook.com <duartecgustavo@outlook.com>
2020-10-25 16:50:53 -0300

    Create produtosTable.sql

---
+++ produtosTable.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM fseletro.produtos
+LIMIT 0, 1000
+
+-- Date: 2020-10-22 08:58
+*/
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (1,'geladeira','Geladeira Brastemp Frost Free Inverse - Prata',5999.00,'imgs/Geladeira_Brastemp_Prata.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (2,'geladeira','Geladeira Consul Frost Free - Prata',4599.00,'imgs/Geladeira_Consul.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (3,'geladeira','Geladeira Brastemp Frost Free Inverse - Branca',4999.00,'imgs/Geladeira_Brastemp_Branca.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (4,'fogoes','Fogão Atlas 4 Bocas Tropical Glass - Branco',699.00,'imgs/Fog%C3%A3o_Atlas.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (5,'fogoes','Fogão Consul 4 Bocas com Acendimento Automático',499.00,'imgs/Fog%C3%A3o_Consul.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (6,'lavalouca','Máquina de Lavar 15kg Enxágue Anti-Alérgico',1999.00,'imgs/Lava_Roupa_Brastemp.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (7,'lavalouca','Máquina de Lavar Philco e Enxágue rápido',1899.00,'imgs/Lava_Roupa_Philco.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (8,'lavalouca','Máquina de Lavar Brastemp e Lavagem mais rápida',1499.00,'imgs/Lavadoura_Brastemp.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (9,'microondas','Micro-ondas Electrolux MEF41 menoe radioativo',499.00,'imgs/Micro_ElectroLux.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (10,'microondas','Micro-ondas Consul ainda menos radioativo',699.00,'imgs/Micro_Ondas_Consul.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (11,'microondas','Micro-ondas Philco sem radiação 2020',250.00,'imgs/Micro_Philco.jpeg');
+INSERT INTO `` (`idprodutos`,`nome_produto`,`descrição`,`preço`,`imagem`) VALUES (12,'freezer','Freezer Electrolux FE26 Cycle Defrost - 203L',999.00,'imgs/Freezer_Electrolux.jpeg');
